4186296af9b34228f459404ac46371a0e079e2a0
Adarsh <vivek.adarsh@capgemini.com>
2024-06-12 15:43:09 +0530

    Second Commit

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,6 @@
 public HelloGit{
 	public static voide main(String[] args){
-		System.out.println("Hello Git I am your freind Java.")
+		System.out.println("Hello Git I am your freind Java.");
+		System.out.println("MergeConflict");
 	}
 }
